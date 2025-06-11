625ae2c371c3f6738262888ce2daccdc3c2c60ec
Apex <32912837+ApexOrder@users.noreply.github.com>
2025-06-11 19:46:48 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD2SFOy3svupb77FTTlJxfgox88cvracKw",
+  authDomain: "apexorder-5ec26.firebaseapp.com",
+  projectId: "apexorder-5ec26",
+  storageBucket: "apexorder-5ec26.appspot.com",
+  messagingSenderId: "624282900955",
+  appId: "1:624282900955:web:4bc2b028364c7ac88aeffe",
+  measurementId: "G-T67D8V3ZKR"
 };
 
 const app = initializeApp(firebaseConfig);
